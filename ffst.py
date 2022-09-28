76ad9428dd278e1cdc75fdec8ae1b4e486701eb2
Ayoola Tosin <66890167+Tosin2289@users.noreply.github.com>
2022-09-29 00:31:56 +0100

    Add files via upload

---
+++ ffst.py
@@ -0,0 +1,15 @@
+from ursina import *            # import everything we need with one line.
+import os
+
+app = Ursina()
+ground = Entity(
+    model = 'cube',
+    color = color.magenta,
+    z = -.1,
+    y = -3,
+    origin = (0, .5),
+    scale = (50, 1, 10),
+    collider = 'box'
+    )
+
+app.run()                       # opens a window and starts the game.
